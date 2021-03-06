95ba04052a40e08a09a70b229ba0fd2fbcd7e471
Athena <athena.partch@gmail.com>
2021-03-05 21:38:11 -0800

    login verification

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
+
 router.post('/', (req, res, next) => {
-    res.json({
-        data: req.body
+    const email = req.body.email
+    User.find({email: email}, (err, users) => {
+        if(err){
+            res.json({
+                confirmation: 'fail',
+                error:err
+            })
+            return
+        }
+        if (users.length == 0){
+            res.json({
+                confirmation: 'fail',
+                error: 'User not found'
+            })
+            return
+        }
+        res.json({
+            confirmation: 'success',
+            user: users
+        })
     })
 });
 
